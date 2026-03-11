988e6ac783c45466478664e9caabe22243d49a52
Michele Oliveira <shellyguns@gmail.com>
2026-03-11 20:35:31 -0300

    Preparação CI

--- src/test/java/br/com/banco/processamento_encargos/ProcessamentoEncargosApplicationTests.java
+++ src/test/java/br/com/banco/processamento_encargos/ProcessamentoEncargosApplicationTests.java
@@ -1,5 +1,6 @@
 package br.com.banco.processamento_encargos;
 
+import br.com.banco.processamento_encargos.domain.exemplo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,16 @@ class ProcessamentoEncargosApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void applicationMainRuns() {
+		ProcessamentoEncargosApplication.main(new String[]{"--spring.main.web-application-type=none"});
+	}
+
+	@Test
+	void exemploMainRuns() {
+		exemplo.main(new String[]{});
 	}
 
 }
