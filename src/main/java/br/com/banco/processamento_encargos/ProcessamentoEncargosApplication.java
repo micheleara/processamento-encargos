a69b20d1b64b4655b318c6e9e8eb3530ad01540c
Michele Oliveira <shellyguns@gmail.com>
2026-03-11 17:56:55 -0300

    Commit inicial

---
+++ src/main/java/br/com/banco/processamento_encargos/ProcessamentoEncargosApplication.java
@@ -0,0 +1,13 @@
+package br.com.banco.processamento_encargos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessamentoEncargosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessamentoEncargosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/banco/processamento_encargos/ProcessamentoEncargosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.banco.processamento_encargos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessamentoEncargosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
